7396cdc1d187b3bfed52177ed15f1c10451d93c2
Aleksei Lekomtsev <alexey.lekomcev@yandex.ru>
2023-06-19 15:17:51 +0300

    sprint 14, ver. 5

--- src/main/java/ru/practicum/shareit/user/UserUserDtoMapper.java
+++ src/main/java/ru/practicum/shareit/user/UserUserDtoMapper.java
@@ -6,5 +6,6 @@ import ru.practicum.shareit.user.dto.UserDto;
 @Mapper(componentModel = "spring")
 public interface UserUserDtoMapper {
     User toUser(UserDto userDto);
+
     UserDto toDto(User user);
 }
